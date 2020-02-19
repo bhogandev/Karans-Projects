dc76d7efa9c7dada04c281e983348998b23856f3
Brandon Hogan <bhogandev@outlook.com>
2020-02-18 23:02:11 -0500

    Added Find e to the Nth Digit Project

---
+++ Numbers/FindetotheNthDigit/FindetotheNthDigit/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FindetotheNthDigit
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(FindE(2.0192837483920103, 5));
+        }
+
+        public static string FindE(double number, int numPlaces)
+        {
+            string e;
+
+            if (number.GetType() == typeof(double) && numPlaces.GetType() == typeof(int))
+            {
+                e = Math.Round(number, numPlaces).ToString();
+            } else
+            {
+                e = "An error has occured. Make sure you've entered a double and a positive integer";
+            }
+
+            return e;
+        }
+    }
+}
