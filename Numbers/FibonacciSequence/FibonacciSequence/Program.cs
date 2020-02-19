1bc8ab35befad6525a092ef48bbd2a650768cab8
Brandon Hogan <bhogandev@outlook.com>
2020-02-18 23:22:27 -0500

    Added Fibonacci Sequence Project

---
+++ Numbers/FibonacciSequence/FibonacciSequence/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FibonacciSequence
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Fibonacci(/*Enter Value Here*/));
+        }
+
+        public static string Fibonacci(int numFib)
+        {
+            string fibSeq = "0 1";
+
+            int temp1 = 0, temp2 = 1, temp3;
+
+            for(int i = 2; i < numFib; i++)
+            {
+                temp3 = temp1 + temp2;
+                temp1 = temp2;
+                temp2 = temp3;
+
+                fibSeq += $" {temp3}";
+            }
+
+            return fibSeq;
+        }
+    }
+}
